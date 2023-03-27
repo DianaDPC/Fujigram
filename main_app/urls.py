705333e575b9a53438592004671880aac5d9ad44
Steve <marlumpy@gmail.com>
2023-03-27 16:38:53 -0400

    post functionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,10 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('accounts/signup/',views.signup, name='signup'),
+    # posts paths
+    path('posts/',views.posts_index, name='posts_index'),
+    path('posts/<int:post_id>/',views.post_details, name='post_details'),
+    path('posts/create/', views.PostCreate.as_view(), name='post_create'),
+    path('posts/<int:pk>/update/', views.PostUpdate.as_view(), name='post_update'),
+    path('posts/<int:pk>/delete/', views.PostDelete.as_view(), name='post_delete'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render, redirect
-
-
-
-
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 # to be implemented later with other routes
@@ -10,26 +7,6 @@ from django.contrib.auth.forms import UserCreationForm
 # from django.contrib.auth.mixins import LoginRequiredMixin 
 
 from .models import Recipe, Post, Comment
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 def home(request):
   return render(request, 'home.html')
@@ -47,3 +24,25 @@ def signup(request):
   form = UserCreationForm()
   context = {'form': form, 'error_message': error_message}
   return render(request, 'registration/signup.html', context)
+
+def posts_index(request):
+  posts=Post.objects.all()
+  return render(request, 'posts/index.html', {'posts':posts})
+
+def post_details(request, post_id):
+  post=Post.objects.get(id=post_id)
+  return render(request, 'posts/details.html', {'post':post})
+
+class PostCreate(CreateView):
+  model = Post
+  fields = ['camera_used', 'photo', 'msg_body']
+  success_url = '/posts/'
+
+class PostUpdate(UpdateView):
+  model = Post
+  fields = ['camera_used', 'msg_body']
+  success_url = '/posts/'
+
+class PostDelete(DeleteView):
+  model = Post
+  success_url = '/posts/'
