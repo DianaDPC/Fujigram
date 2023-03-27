62d5c37efacd4ecf5c6c01e3f13a83e287f6fb4e
Diana Chan <dianachan@Dianas-MacBook-Pro.local>
2023-03-27 16:58:43 -0400

    adding nav and templates

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('accounts/signup/',views.signup, name='signup'),
+    path('posts/', views.posts_index, name='index'),
+    path('posts/<int:post_id>/', views.posts_detail, name='detail'),
+    path('posts/create/', views.PostCreate.as_view(), name='posts_create'),
+    path('posts/<int:pk>/update/', views.PostUpdate.as_view(), name='posts_update'),
+    path('posts/<int:pk>/delete/', views.PostDelete.as_view(), name='posts_delete'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,38 +1,37 @@
 from django.shortcuts import render, redirect
-
-
-
-
-from django.contrib.auth import login
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
-# to be implemented later with other routes
-# from django.contrib.auth.decorators import login_required
-# from django.contrib.auth.mixins import LoginRequiredMixin 
-
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
 from .models import Recipe, Post, Comment
 
 
 
+# Create your views here.
 
+class PostCreate(LoginRequiredMixin, CreateView):
+  model = Post
+  fields = '__all__'
 
+class PostUpdate(LoginRequiredMixin, UpdateView):
+  model = Post
+  fields = '__all__'
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+class PostDelete(LoginRequiredMixin, DeleteView):
+  model = Post
+  success_url = '/posts/'
 
 def home(request):
   return render(request, 'home.html')
+
+def posts_index(request):
+  posts = Post.objects.all()
+  return render(request, 'posts/index.html', { 'posts': posts })
+
+def posts_detail(request, post_id):
+  post = Post.objects.get(id=post_id)
+  return render(request, 'posts/detail.html', { 'post': post })
 
 def signup(request):
   error_message = ''
