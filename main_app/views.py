cce0921be3406aab04c56d640df91b3eb9eaab38
dodoslayer98 <123933669+dodoslayer98@users.noreply.github.com>
2023-03-27 12:46:25 -0400

    added recipe templates

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+from django.forms import ModelForm
+from .models import Recipe, Post, Comment
+
 
 # Create your views here.
+def posts_index(request):
+  posts = Post.objects.all()
+  return render(request, 'posts/index.html', { 'posts': posts })
+
+def posts_detail(request, post_id):
+  post = Post.objects.get(id=post_id)
+  return render(request, 'posts/detail.html', { 'post': post })
+
+class PostCreate(CreateView):
+    model = Post
+    fields = '__all__'
+
+class RecipeCreate(CreateView):
+   model = Post
+   fields = '__all__'
