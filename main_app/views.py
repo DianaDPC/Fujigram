3afc4e5f3bfec3655a942b2d9435333498de57d8
dodoslayer98 <123933669+dodoslayer98@users.noreply.github.com>
2023-03-27 17:20:51 -0400

    update before making a view for all recipes

--- main_app/views.py
+++ main_app/views.py
@@ -29,7 +29,7 @@ def signup(request):
 class RecipeCreate(CreateView):
   model = Recipe
   fields = ['name','sensor','dynamic_range','film_simulation','monochromatic_color_WC','monochromatic_color_MG','highlight_tone','shadow_tone','color','noise_reduction','clarity','grain_effect','grain_size','color_chrome_effect','white_balance','white_balance_shift_red','white_balance_shift_blue','sharpness','long_exposure_nr','lens_modulation_optimizer','color_space','iso','exposure_compensation']
-
+  
   def form_valid(self, form):
     form.instance.user_id = self.request.user
     return super().form_valid(form)
