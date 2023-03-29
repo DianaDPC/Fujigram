6418562c799c2aa5b4561988b72124c541bc2429
Steve <marlumpy@gmail.com>
2023-03-29 16:12:35 -0400

    fix search

--- fujigram/settings.py
+++ fujigram/settings.py
@@ -83,10 +83,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'fujigram',
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT')
+        'USER': 'jmbpe',
+        'PASSWORD': 'password'
     }
 }
 
